8f47ab646518f28b0a029ea731cfbea1dbb9f5c9
Yessen Murat <shoyo2209@gmail.com>
2023-02-18 18:35:40 +0600

    Generate random number simple version

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const generateForm = document.getElementById("generateForm");
+const outputField = document.getElementById("output");
 
-generateForm.onclick = (event) => {
+generateForm.onsubmit = (event) => {
   event.preventDefault();
+
+  const formData = new FormData(generateForm);
+  const boundary = Object.fromEntries(formData.entries());
+  const maxValue = Number(boundary.lower_limit);
+  const minValue = Number(boundary.upper_limit);
+  const randomNumber = generateRandomNumber(maxValue, minValue);
+  outputField.textContent = `Random Number: ${randomNumber}`;
 };
+
+function generateRandomNumber(max, min) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+}
